75a05df0326723793b83e061a58463a6aaa2a123
Oliver Browning <lemenade3@hotmail.com>
2023-01-26 23:01:18 +0000

    most tests added, only analyseArray left

--- index.js
+++ index.js
@@ -1,12 +1,88 @@
 function capitalize(string) {
     let array = Array.from(string)
+    for (let i = 0; i < array.length; i++) {
+        array[i] = array[i].toLowerCase()
+    }
     array[0] = array[0].toUpperCase()
     let concatArray = array.join('')
     return concatArray
 }
 
 function reverseString(string) {
-    return 'sniggum'
+    let array = Array.from(string)
+    let reverseArray = array.reverse()
+    let reversedString = reverseArray.join('')
+    return reversedString
 }
 
-export {capitalize, reverseString}
+const calculator = {
+    add: function(a, b) {
+        if (isNaN(a) || isNaN(b)) {
+            return 'Error'
+        } else {
+            return a + b
+        }
+    },
+
+    subtract: function(a, b) {
+        if (isNaN(a) || isNaN(b)) {
+            return 'Error'
+        } else {
+            return a - b
+        }
+    },
+
+    divide: function(a, b) {
+        if (isNaN(a) || isNaN(b)) {
+            return 'Error'
+        } else {
+            return a / b
+        }
+    },
+
+    multiply: function(a, b) {
+        if (isNaN(a) || isNaN(b)) {
+            return 'Error'
+        } else {
+            return a * b
+        }
+    },
+
+    caeserCipher: function(string, n) {
+        let array = Array.from(string)
+        const special = /[` !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        for (let i = 0; i < array.length; i++) {
+            let code = array[i].charCodeAt(0)
+            let newCode = code + n
+            while (code <= 122 && newCode > 122) {
+                newCode -= 26
+            }
+            while (code >= 97 && newCode < 97) {
+                newCode += 26
+            }
+            while (code <= 90 && newCode > 90) {
+                newCode -= 26
+            }
+            while (code >= 65 && newCode < 65) {
+                newCode += 26
+            }
+            if (special.test(array[i])) {
+                newCode = code
+            }
+            if (code >= 48 && code <= 57) {
+                newCode = code
+            }
+            array[i] = String.fromCharCode(newCode)
+        }
+        let cipher = array.join('')
+        return cipher
+    }
+}
+
+function analyseArray(array) {
+    let object = {}
+    return object
+}
+
+
+export {capitalize, reverseString, calculator, analyseArray}
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import {capitalize, reverseString} from './index'
+import {capitalize, reverseString, calculator, analyseArray} from './index'
 
 // Tests capitalize
 
@@ -10,6 +10,14 @@ test('Returns alternative string with first letter capitalized', () => {
     expect(capitalize('clifford')).toBe('Clifford')
 })
 
+test('Works if already capitalized', () => {
+    expect(capitalize('Clifford')).toBe('Clifford')
+})
+
+test('Converts other letters to lower case', () => {
+    expect(capitalize('cliFfOrd')).toBe('Clifford')
+})
+
 // Tests reverseString
 
 test('Reverses a string', () => {
@@ -18,4 +26,102 @@ test('Reverses a string', () => {
 
 test('Reverses a string', () => {
     expect(reverseString('cats and dogs')).toBe('sgod dna stac')
+})
+
+// Tests Calculator
+
+test('Calculator is an Object', () => {
+    expect(typeof calculator === 'object').toBeTruthy()
+})
+
+// Tests Add
+
+test('Calculator adds', () => {
+    expect(calculator.add(2, 5)).toBe(7)
+})
+
+test('Calculator add rejects NaN', () => {
+    expect(calculator.add(2, "r")).toBe('Error')
+})
+
+test('Add works with negative numbers', () => {
+    expect(calculator.add(2, -5)).toBe(-3)
+})
+
+// Tests Subtract
+
+test('Calculator subtracts', () => {
+    expect(calculator.subtract(5, 2)).toBe(3)
+})
+
+test('Calculator subtract rejects NaN', () => {
+    expect(calculator.subtract(2, 'M')).toBe('Error')
+})
+
+test('Calculator subtract works with negative numbers', () => {
+    expect(calculator.subtract(2, -3)).toBe(5)
+})
+
+// Tests Divide
+
+test('Calculator divides', () => {
+    expect(calculator.divide(4, 2)).toBe(2)
+})
+
+test('Calculator divides rejects NaN', () => {
+    expect(calculator.divide(4, 'W')).toBe("Error")
+})
+
+test('Calculator divides negative numbers', () => {
+    expect(calculator.divide(4, -2)).toBe(-2)
+})
+
+// Tests Multiply
+
+test('Calculator multiplies', () => {
+    expect(calculator.multiply(4, 2)).toBe(8)
+})
+
+test('Calculator multiply rejects NaN', () => {
+    expect(calculator.multiply(4, 'W')).toBe("Error")
+})
+
+test('Calculator multiplies negative numbers', () => {
+    expect(calculator.multiply(4, -2)).toBe(-8)
+})
+
+// Tests caeserCipher
+
+test('caeserCipher', () => {
+    expect(calculator.caeserCipher('Hello', 2)).toBe('Jgnnq')
+})
+
+test('caeserCipher works with wrapping', () => {
+    expect(calculator.caeserCipher('Zoom', 2)).toBe('Bqqo')
+})
+
+test('caeserCipher works with negative numbers', () => {
+    expect(calculator.caeserCipher('Zoom', -2)).toBe('Xmmk')
+})
+
+test('caeserCipher works with negative wrapping', () => {
+    expect(calculator.caeserCipher('About', -2)).toBe('Yzmsr')
+})
+
+test('caeserCipher works with large numbers', () => {
+    expect(calculator.caeserCipher('confidential', 256)).toBe('ykjbezajpewh')
+})
+
+test('caeserCipher ignores special characters', () => {
+    expect(calculator.caeserCipher("We're having a good time!", 5)).toBe("Bj'wj mfansl f ltti ynrj!")
+})
+
+test('caeserCipher ignores numbers', () => {
+    expect(calculator.caeserCipher("5", 5)).toBe("5")
+})
+
+// Tests analyseArray
+
+test('analyzeArray returns an object', () => {
+    expect(typeof analyseArray([1,2,3,4]) === 'object').toBeTruthy()
 })
